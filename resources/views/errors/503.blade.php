5877d6a00ebe27c93c555ff772436935f1d21583
richmartins <richard.tenorio@excellgene.com>
2023-01-09 20:36:48 +0100

    add maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mode maintenance</title>
+    <style>
+        @font-face {
+            font-family: "Euclid";
+            src: url("/storage/font/EuclidSquare-Light-WebS.woff2") format("woff2"),
+            url("/storage/font/EuclidSquare-Light-WebS.woff") format("woff");
+        }
+
+        @font-face {
+            font-family: "Euclid";
+            font-weight: bold;
+            src: url("/storage/font/EuclidSquare-SemiBold-WebS.woff2") format("woff2"),
+            url("/storage/font/EuclidSquare-SemiBold-WebS.woff") format("woff");
+        }
+
+        main {
+            font-family: "Euclid", sans-serif;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 100%;
+            min-heigth: 100svh;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>Error 503, :-(</h1>
+        <p>Oops, something went wrong !</p>
+        <p>Everything will be back soon</p>
+    </main>
+</body>
+</html>
